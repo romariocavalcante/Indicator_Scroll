feb9b2a328148e740506a044d495094a35dff705
cavalcanteromario <romario.cavalcante.idcorpbrasil@gmail.com>
2021-10-27 15:07:55 -0300

    first commit

---
+++ Readme.md
@@ -0,0 +1,6 @@
+# Indicador de Scroll
+####
+
+<video controls autoplay>
+  <source src="indicator.mp4" type="video/mp4">
+</video>
---
+++ script.js
@@ -0,0 +1,7 @@
+const indicator = document.querySelector('#indicator'); 
+const maxHeight = document.body.scrollHeight - window.innerHeight;
+
+window.addEventListener('scroll', () => {
+    const percentage = ( window.scrollY / maxHeight ) * 100;
+    indicator.style.width = `${percentage}%`;
+});
